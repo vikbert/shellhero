d3053c163d9500c2ee2395ee5b4fa7b55a1e30f4
vikbert <segentor@gmail.com>
2021-10-13 00:41:50 +0200

    updates

--- src/git_branch_clean.sh
+++ src/git_branch_clean.sh
@@ -1,5 +1,40 @@
 #!/usr/bin/env bash
-source print.sh
+GREEN='\033[32m' # success
+CYAN='\033[0;36m' # info
+GRAY='\033[0;37m' # info
+YELLOW='\033[1;33m' # warning
+RED='\033[1;31m' # error
+NC='\033[0m' # No Color
+bold=$(tput bold)
+normal=$(tput sgr0)
+
+print_success() {
+    message="$1"
+    echo
+    echo -e "✅ $GREEN$message$NC";
+}
+
+print_info() {
+    message="$1"
+    echo
+    echo -e "🗣  $CYAN$message$NC";
+}
+
+print_warning() {
+    message="$1"
+    echo
+    echo -e "🔅 $YELLOW$message$NC";
+}
+
+print_error() {
+    message="$1"
+    echo
+    echo -e "❌ $RED$message$NC";
+}
+
+print_hr() {
+  echo -e "----------------------------------------------"
+}
 
 PRIMARY_BRANCH='main'
 DRY_RUN=false
